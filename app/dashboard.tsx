af53a8c0904c3c0770cb897454b04a92527d775a
Brennan Moore <brennanmoore@gmail.com>
2020-06-03 14:14:46 -0400

    add prettier config and render docs

--- app/dashboard.tsx
+++ app/dashboard.tsx
@@ -1,19 +1,21 @@
-import Box from "@material-ui/core/Box";
-import Container from "@material-ui/core/Container";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
-import clsx from "clsx";
-import React, { useState } from "react";
-import { styles } from "./app";
-import Copyright from "./copyright";
-import { fetchFromGoogle } from "./fetch";
-import LeftDrawer from "./left-drawer";
-import TopBar from "./top-bar";
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import clsx from 'clsx';
+import { differenceInCalendarDays } from 'date-fns';
+import React, { useState } from 'react';
+import { styles } from './app';
+import Copyright from './copyright';
+import Docs, { doc } from './docs';
+import { fetchFromGoogle } from './fetch';
+import LeftDrawer from './left-drawer';
+import TopBar from './top-bar';
 
 // Note: Lots more info on this object but is unused by the app
 const initialGoogleState = {
-  accessToken: "",
+  accessToken: '',
 };
 
 export type googleState = typeof initialGoogleState;
@@ -21,16 +23,16 @@ export type googleState = typeof initialGoogleState;
 const loadLibraries = () => {
   gapi.client.init({
     discoveryDocs: [
-      "https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest",
-      "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest",
-      "https://www.googleapis.com/discovery/v1/apis/driveactivity/v2/rest",
-      "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
+      'https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest',
+      'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest',
+      'https://www.googleapis.com/discovery/v1/apis/driveactivity/v2/rest',
+      'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest',
     ],
   });
   return null;
 };
 
-gapi.load("client", loadLibraries);
+gapi.load('client', loadLibraries);
 
 const listDriveActivity = async () =>
   // Todo: Make driveactivity types
@@ -39,14 +41,15 @@ const listDriveActivity = async () =>
   });
 
 const listDriveFiles = async () =>
+  // Does not allow filtering by modified time OR deleted
   await gapi.client.drive.files.list({
     includeItemsFromAllDrives: true,
     includeTeamDriveItems: true,
     supportsAllDrives: true,
     supportsTeamDrives: true,
-    orderBy: "modifiedTime desc",
+    orderBy: 'modifiedTime desc',
     pageSize: 10,
-    fields: "nextPageToken, files(id, name)",
+    fields: 'nextPageToken, files(id, name, webViewLink, shared, starred, trashed, modifiedTime)',
   });
 
 interface IProps {
@@ -64,22 +67,22 @@ const Dashboard = (props: IProps) => {
     setOpen(false);
   };
 
-  const [driveFiles] = fetchFromGoogle("drive", listDriveFiles(), {});
-  const [activity] = fetchFromGoogle("activity", listDriveActivity(), {});
+  const [driveResponse] = fetchFromGoogle('drive', listDriveFiles(), {});
+  const [activity] = fetchFromGoogle('activity', listDriveActivity(), {});
+
+  const filteredDriveFiles =
+    driveResponse && driveResponse.data && driveResponse.data.files
+      ? driveResponse.data.files.filter(
+          (file: doc) =>
+            !file.trashed && differenceInCalendarDays(new Date(), new Date(file.modifiedTime)) < 30,
+        )
+      : [];
 
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <TopBar
-        classes={classes}
-        handleDrawerOpen={handleDrawerOpen}
-        isOpen={isOpen}
-      />
-      <LeftDrawer
-        classes={classes}
-        handleDrawerClose={handleDrawerClose}
-        isOpen={isOpen}
-      />
+      <TopBar classes={classes} handleDrawerOpen={handleDrawerOpen} isOpen={isOpen} />
+      <LeftDrawer classes={classes} handleDrawerClose={handleDrawerClose} isOpen={isOpen} />
       <main className={classes.content}>
         <div className={classes.appBarSpacer} />
         <Container maxWidth="lg" className={classes.container}>
@@ -87,14 +90,15 @@ const Dashboard = (props: IProps) => {
             <Grid item xs={12} md={8} lg={9}>
               <Paper className={fixedHeightPaper}>
                 <pre>ACTIVITY:{JSON.stringify(activity)}</pre>
-                <pre>DRIVE FILES:{JSON.stringify(driveFiles)}</pre>
               </Paper>
             </Grid>
             <Grid item xs={12} md={4} lg={3}>
               <Paper className={fixedHeightPaper}>Other stuff</Paper>
             </Grid>
             <Grid item xs={12}>
-              <Paper className={classes.paper}>more stuff</Paper>
+              <Paper className={classes.paper}>
+                <Docs docs={filteredDriveFiles} />
+              </Paper>
             </Grid>
           </Grid>
           <Box pt={4}>
---
+++ app/docs.tsx
@@ -0,0 +1,55 @@
+import Link from '@material-ui/core/Link';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Typography from '@material-ui/core/Typography';
+import { formatRelative } from 'date-fns';
+import React from 'react';
+
+export type doc = {
+  id: string;
+  name: string;
+  webViewLink: string;
+  shared: boolean;
+  starred: boolean;
+  trashed: boolean;
+  modifiedTime: string;
+};
+
+interface IProps {
+  docs: doc[];
+}
+
+const Docs = (props: IProps) => (
+  <React.Fragment>
+    <Typography component="h2" variant="h6" color="primary" gutterBottom>
+      Recent Google Docs
+    </Typography>
+    <Table size="small">
+      <TableHead>
+        <TableRow>
+          <TableCell>Date</TableCell>
+          <TableCell>Name</TableCell>
+          <TableCell>Starred</TableCell>
+          <TableCell align="right">Link</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {props.docs.map((row) => (
+          <TableRow key={row.id}>
+            <TableCell>{formatRelative(new Date(row.modifiedTime), new Date())}</TableCell>
+            <TableCell>{row.name}</TableCell>
+            <TableCell>{row.starred}</TableCell>
+            <TableCell align="right">
+              <Link href={row.webViewLink}>Link</Link>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </React.Fragment>
+);
+
+export default Docs;
