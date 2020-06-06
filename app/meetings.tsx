ed07895e080587d5acab9efa488db049b44f5e7e
Brennan Moore <brennanmoore@gmail.com>
2020-06-05 21:05:09 -0400

    display emails and docs on calendar events

--- app/meetings.tsx
+++ app/meetings.tsx
@@ -1,3 +1,4 @@
+import Avatar from '@material-ui/core/Avatar';
 import Box from '@material-ui/core/Box';
 import Collapse from '@material-ui/core/Collapse';
 import Grid from '@material-ui/core/Grid';
@@ -15,8 +16,49 @@ import { format } from 'date-fns';
 import { uniqBy } from 'lodash';
 import React, { useState } from 'react';
 import { IProps } from './dashboard';
+import { IFormattedDriveActivity } from './fetch/fetch-first';
 import { formattedEmail } from './fetch/fetch-second';
+import PersonDataStore from './store/person-store';
 import { ISegment } from './store/time-store';
+
+const Activity = (props: { activity: IFormattedDriveActivity; personStore: PersonDataStore }) => {
+  const person = props.activity.actorPersonId
+    ? props.personStore.getPersonByPeopleId(props.activity.actorPersonId)
+    : { name: 'unknown', id: 'blank' };
+  return (
+    <TableRow>
+      <TableCell style={{ width: '15%' }}>
+        <Typography variant="caption">{person.name || person.id}</Typography>
+      </TableCell>
+      <TableCell component="th" scope="row">
+        <Typography variant="caption">
+          <Link color="textPrimary" target="_blank" href={props.activity.link || ''}>
+            <b>{props.activity.title}</b>
+          </Link>
+        </Typography>
+      </TableCell>
+    </TableRow>
+  );
+};
+
+const DriveActivityForSegment = (props: { segment: ISegment; personStore: PersonDataStore }) => {
+  const actions = uniqBy(props.segment.driveActivity, 'link');
+  if (actions.length < 1) {
+    return null;
+  }
+  return (
+    <React.Fragment>
+      <Typography variant="h6">Documents</Typography>
+      <Table size="small">
+        <TableBody>
+          {actions.map((action) => (
+            <Activity key={action.id} activity={action} personStore={props.personStore} />
+          ))}
+        </TableBody>
+      </Table>
+    </React.Fragment>
+  );
+};
 
 const Email = (props: { email: formattedEmail }) => {
   const emailLink = `https://mail.google.com/mail/u/0/#inbox/${props.email.id}`;
@@ -62,11 +104,21 @@ const useRowStyles = makeStyles((theme) => ({
       borderBottom: 'unset',
     },
   },
+  secondary: {
+    color: theme.palette.getContrastText(theme.palette.secondary.main),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  avatarContainer: {
+    width: 42,
+    paddingLeft: 0,
+    paddingRight: 0,
+  },
 }));
 
-const Row = (props: { row: ISegment }) => {
+const Row = (props: { row: ISegment; personStore: PersonDataStore }) => {
   const [isOpen, setOpen] = useState(false);
   const classes = useRowStyles();
+  const actionCount = props.row.driveActivity.length + props.row.emails.length;
   return (
     <React.Fragment>
       <TableRow className={classes.root} hover onClick={() => setOpen(!isOpen)}>
@@ -86,7 +138,10 @@ const Row = (props: { row: ISegment }) => {
             </Link>
           </Typography>
         </TableCell>
-        <TableCell align="right">
+        <TableCell align="right" className={classes.avatarContainer}>
+          <Avatar className={classes.secondary}>{actionCount}</Avatar>
+        </TableCell>
+        <TableCell align="right" className={classes.avatarContainer}>
           <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!isOpen)}>
             {isOpen ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
           </IconButton>
@@ -97,6 +152,7 @@ const Row = (props: { row: ISegment }) => {
           <Collapse in={isOpen} timeout="auto" unmountOnExit>
             <Box margin={1}>
               <EmailsForSegment segment={props.row} />
+              <DriveActivityForSegment segment={props.row} personStore={props.personStore} />
             </Box>
           </Collapse>
         </TableCell>
@@ -113,7 +169,7 @@ const Meetings = (props: IProps) => {
         <Table size="small">
           <TableBody>
             {meetings.map((row) => (
-              <Row key={row.id} row={row} />
+              <Row key={row.id} row={row} personStore={props.personDataStore} />
             ))}
           </TableBody>
         </Table>
