54aa29432ecccff7e9e01b44e99300a3dd7c305c
Brennan Moore <brennanmoore@gmail.com>
2020-06-20 11:14:08 -0400

    maybe

--- app/nav/people-list.tsx
+++ app/nav/people-list.tsx
@@ -3,7 +3,7 @@ import Avatar from '@material-ui/core/Avatar';
 import Grid from '@material-ui/core/Grid';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import { chunk } from 'lodash';
+import { chunk, sortBy } from 'lodash';
 import React from 'react';
 import { IPerson } from '../store/person-store';
 
@@ -14,7 +14,7 @@ interface IProps {
   people?: IPerson[] | null;
 }
 
-const peopleRowHtml = (people: IPerson[], handlePersonClick: (id: string) => void) =>
+const renderPeopleRow = (people: IPerson[], handlePersonClick: (id: string) => void) =>
   people.map((person) => (
     <Grid item xs={4} key={person.id} onClick={() => handlePersonClick(person.emailAddress)}>
       <ListItem button>
@@ -30,16 +30,16 @@ const peopleRowHtml = (people: IPerson[], handlePersonClick: (id: string) => voi
     </Grid>
   ));
 
-const peopleHtml = (people: IPerson[], handlePersonClick: (id: string) => void) =>
+const renderPeopleGrid = (people: IPerson[], handlePersonClick: (id: string) => void) =>
   chunk(people, ROW_COUNT).map((personRow, index) => (
     <Grid key={index} container item xs={12} spacing={3}>
-      {peopleRowHtml(personRow, handlePersonClick)}
+      {renderPeopleRow(personRow, handlePersonClick)}
     </Grid>
   ));
 
 const PeopleList = (props: IProps) => (
   <Grid container spacing={1}>
-    {peopleHtml(props.people || [], props.handlePersonClick)}
+    {renderPeopleGrid(props.people ? sortBy(props.people, 'name') : [], props.handlePersonClick)}
   </Grid>
 );
 
