73c5721fec79205b63af29e1874e423b0730a662
Brennan Moore <brennanmoore@gmail.com>
2020-07-21 16:54:40 -0400

    more homepage stuff

--- app/homepage/homepage.tsx
+++ app/homepage/homepage.tsx
@@ -21,6 +21,11 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.secondary.main,
     width: '100%',
   },
+  info: {
+    marginTop: theme.spacing(9),
+    padding: theme.spacing(9),
+    width: '100%',
+  },
   submit: {
     margin: theme.spacing(4, 0, 2),
     padding: theme.spacing(2, 6),
@@ -39,8 +44,11 @@ const useStyles = makeStyles((theme) => ({
   plan: {
     marginTop: theme.spacing(2),
   },
-  image: {},
+  image: { width: '100%', maxWidth: '586px' },
   primaryDark: { color: theme.palette.primary.dark },
+  row: {
+    borderBottom: `1px solid ${theme.palette.primary.dark}`,
+  },
 }));
 
 const HomePage = (props: { setGoogleLoginState: (response: any) => void }) => {
@@ -102,6 +110,51 @@ const HomePage = (props: { setGoogleLoginState: (response: any) => void }) => {
         </Grid>
         <Grid item xs={6}>
           <img className={classes.image} src={`${config.DOMAIN}/images/designer_file_case.png`} />
+        </Grid>
+      </Grid>
+      <Grid container className={classes.info} alignItems="center">
+        <Grid item xs={6}>
+          <Container maxWidth="xs">
+            <Typography variant="h3" className={classes.primaryDark}>
+              Your meeting helper
+            </Typography>
+          </Container>
+        </Grid>
+        <Grid item xs={6}>
+          <Container maxWidth="xs">
+            <Grid container spacing={5}>
+              <Grid item className={classes.row}>
+                <Typography variant="h6">
+                  Kelp does not store your data or send your data to any third parties.
+                </Typography>
+                <Typography variant="body1">
+                  Kelp brings your data together and organizes it to be simple and easy to
+                  understand. Kelp infers associations between information, such as between a
+                  person, a meeting with the person and document edits by the person.
+                </Typography>
+              </Grid>
+              <Grid item className={classes.row}>
+                <Typography variant="h6">
+                  Kelp does not store your data or send your data to any third parties.
+                </Typography>
+                <Typography variant="body1">
+                  Kelp brings your data together and organizes it to be simple and easy to
+                  understand. Kelp infers associations between information, such as between a
+                  person, a meeting with the person and document edits by the person.
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="h6">
+                  Kelp does not store your data or send your data to any third parties.
+                </Typography>
+                <Typography variant="body1">
+                  Kelp brings your data together and organizes it to be simple and easy to
+                  understand. Kelp infers associations between information, such as between a
+                  person, a meeting with the person and document edits by the person.
+                </Typography>
+              </Grid>
+            </Grid>
+          </Container>
         </Grid>
       </Grid>
       <Footer />
