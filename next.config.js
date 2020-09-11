22e39f7451bd994898ddd05abfe31c6c7085ebc3
Brennan Moore <brennanmoore@gmail.com>
2020-09-11 15:41:35 -0400

    add security headers

--- next.config.js
+++ next.config.js
@@ -1,16 +1,30 @@
-module.exports = {
-  poweredByHeader: false,
+const { createSecureHeaders } = require('next-secure-headers');
 
+module.exports = {
   async headers() {
     return [
       {
-        source: '/',
-        headers: [
-          {
-            key: 'strict-transport-security',
-            value: 'max-age=31536000; includeSubDomains; preload',
+        source: '/(.*)',
+        headers: createSecureHeaders({
+          contentSecurityPolicy: {
+            directives: {
+              defaultSrc: "'self'",
+              styleSrc: ["'self'", "'unsafe-inline'"],
+              scriptSrc: ["'unsafe-eval'"],
+              scriptSrcElem: ["'self'", 'https://apis.google.com'],
+              frameSrc: [
+                'https://kelp.us.auth0.com/',
+                'https://content.googleapis.com/',
+                'https://accounts.google.com/',
+                'https://content-gmail.googleapis.com/',
+              ],
+              connectSrc: ["'self'", 'https://kelp.us.auth0.com/oauth/token'],
+            },
           },
-        ],
+          forceHTTPSRedirect: true,
+          referrerPolicy: 'same-origin',
+          xssProtection: 'block-rendering',
+        }),
       },
     ];
   },
