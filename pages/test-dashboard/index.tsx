3dc703c9096f65a17768bf962f8de11e31e4d77c
Brennan Moore <brennanmoore@gmail.com>
2020-09-27 23:57:49 -0400

    wip

--- pages/test-dashboard/index.tsx
+++ pages/test-dashboard/index.tsx
@@ -1,89 +1,15 @@
-import Drawer from '@material-ui/core/Drawer';
-import { makeStyles } from '@material-ui/core/styles';
-import { useRouter } from 'next/router';
-import React, { useState } from 'react';
-import Docs from '../../components/dashboard/docs';
-import Meetings from '../../components/dashboard/meetings';
-import People from '../../components/dashboard/people';
-import ExpandedDocument from '../../components/docs/expand-document';
-import ExpandedMeeting from '../../components/meeting/expand-meeting';
-import LeftDrawer from '../../components/nav/left-drawer';
-import ExpandPerson from '../../components/person/expand-person';
-import panelStyles from '../../components/shared/panel-styles';
+import React from 'react';
 import useStore from '../../components/store/use-fake-store';
+import { DashboardContainer } from '../dashboard/index';
 
-const useStyles = makeStyles((theme) => ({
-  appBarSpacer: theme.mixins.toolbar,
-  container: {
-    display: 'flex',
-  },
-  content: {
-    flexGrow: 1,
-    height: '100vh',
-    overflow: 'auto',
-    background: 'white',
-  },
-}));
-
-const DashboardContainer = () => {
+export const drawerWidth = 240;
+const TestDashboard = () => {
   const store = useStore();
-  const classes = useStyles();
-  const panelClasses = panelStyles();
-  const [isOpen, setOpen] = useState(true);
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
-
-  const handleRefreshClick = () => store.refetch();
-  // todo
-  const slug = useRouter().query.slug as string | null;
-  const tab = useRouter().query.tab as string;
-
-  const tabHash = {
-    meetings: <Meetings {...store} />,
-    docs: <Docs {...store} />,
-    people: <People {...store} />,
-  } as any;
-
-  const expandHash = {
-    docs: slug && <ExpandedDocument documentId={slug} {...store} />,
-    people: slug && <ExpandPerson personId={slug} {...store} />,
-    meetings: slug && <ExpandedMeeting meetingId={slug} {...store} />,
-  } as any;
-
-  // <TopBar handleDrawerOpen={handleDrawerOpen} isOpen={isOpen} /> --!>
-  // add above route component <div className={classes.appBarSpacer} />
   return (
-    <div className={classes.container}>
-      <LeftDrawer
-        tab={tab as any}
-        lastUpdated={store.lastUpdated}
-        handleDrawerClose={handleDrawerClose}
-        isOpen={isOpen}
-        people={store.personDataStore.getPeople()}
-        documents={store.docDataStore.getDocs()}
-        meetings={store.timeDataStore.getSegments()}
-        handleDrawerOpen={handleDrawerOpen}
-        handleRefreshClick={handleRefreshClick}
-      />
-      <main className={classes.content}>
-        <div className={panelClasses.panel}>{tabHash[tab]}</div>
-        <Drawer
-          open={true}
-          classes={{
-            paper: panelClasses.dockedPanel,
-          }}
-          anchor="right"
-          variant="persistent"
-        >
-          {expandHash[tab]}
-        </Drawer>
-      </main>
-    </div>
+    <React.Fragment>
+      <DashboardContainer store={store} />
+    </React.Fragment>
   );
 };
 
-export default DashboardContainer;
+export default TestDashboard;
