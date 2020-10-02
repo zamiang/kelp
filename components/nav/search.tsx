01d129c84243aa0ecdaa41285b8b725f18df4e9b
Brennan Moore <brennanmoore@gmail.com>
2020-10-01 20:31:20 -0400

    doh

--- components/nav/search.tsx
+++ components/nav/search.tsx
@@ -4,7 +4,7 @@ import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 import SearchIcon from '@material-ui/icons/Search';
 import Autocomplete from '@material-ui/lab/Autocomplete';
-import { NextRouter, Router, useRouter } from 'next/router';
+import { NextRouter, useRouter } from 'next/router';
 import React from 'react';
 import { IProps } from './left-drawer';
 
@@ -79,7 +79,6 @@ const Search = (props: IProps) => {
           renderInput={(params) => (
             <TextField placeholder="Search…" className={classes.inputInput} {...params} />
           )}
-          // Typings are wrong for this library
           onChange={handleAutocompleteSelect}
         />
       )}
