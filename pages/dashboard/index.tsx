9d4f887d224a280a697178ac686ad1f8057c7f65
Brennan Moore <brennanmoore@gmail.com>
2020-10-05 21:34:15 -0400

    fixes

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,7 +1,9 @@
 import { withAuthenticationRequired } from '@auth0/auth0-react';
+import { Typography } from '@material-ui/core';
 import Backdrop from '@material-ui/core/Backdrop';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Drawer from '@material-ui/core/Drawer';
+import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Alert from '@material-ui/lab/Alert';
 import { useRouter } from 'next/router';
@@ -36,21 +38,33 @@ const useStyles = makeStyles((theme) => ({
 const useBackdropStyles = makeStyles((theme) => ({
   backdrop: {
     zIndex: theme.zIndex.drawer + 1,
-    color: theme.palette.primary.dark,
   },
 }));
 
 const LoadingDashboardContainer = () => {
   const accessToken = useAccessToken();
-  const classes = useBackdropStyles();
   const store = useStore(accessToken || '');
   return (
     <React.Fragment>
-      <Backdrop className={classes.backdrop} open={!accessToken}>
-        <CircularProgress color="inherit" />
-      </Backdrop>
+      <Loading isOpen={!accessToken} message="Loading" />
       {accessToken && <DashboardContainer store={store} />}
     </React.Fragment>
+  );
+};
+
+const Loading = (props: { isOpen: boolean; message: string }) => {
+  const classes = useBackdropStyles();
+  return (
+    <Backdrop className={classes.backdrop} open={props.isOpen}>
+      <Grid container alignItems="center" justify="center">
+        <Grid item style={{ width: '100%', textAlign: 'center' }}>
+          <CircularProgress color="inherit" />
+        </Grid>
+        <Grid item>
+          <Typography variant="h5">{props.message}</Typography>
+        </Grid>
+      </Grid>
+    </Backdrop>
   );
 };
 
@@ -126,4 +140,6 @@ export const DashboardContainer = ({ store }: IProps) => {
   );
 };
 
-export default LoadingDashboardContainer;
+export default withAuthenticationRequired(LoadingDashboardContainer, {
+  onRedirecting: () => <Loading isOpen={true} message="Authenticating" />,
+});
