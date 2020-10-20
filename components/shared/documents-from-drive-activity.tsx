1f3d87f1562f3e1096350c01eac205ae692921dd
Brennan Moore <brennanmoore@gmail.com>
2020-10-19 22:11:42 -0400

    work on displaying drive activity

--- components/shared/documents-from-drive-activity.tsx
+++ components/shared/documents-from-drive-activity.tsx
@@ -1,11 +1,13 @@
-import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import { formatDistanceToNow } from 'date-fns';
 import { uniqBy } from 'lodash';
+import Link from 'next/link';
 import React from 'react';
 import { IFormattedDriveActivity } from '../fetch/fetch-drive-activity';
+import DocDataStore, { IDoc } from '../store/doc-store';
 import PersonDataStore from '../store/person-store';
 
 const useRowStyles = makeStyles(() => ({
@@ -26,54 +28,53 @@ const useRowStyles = makeStyles(() => ({
   },
 }));
 
-const Activity = (props: {
-  driveActivity: IFormattedDriveActivity;
-  personStore: PersonDataStore;
-}) => {
+const Activity = (props: { document: IDoc }) => {
   const classes = useRowStyles();
-  const person =
-    props.driveActivity.actorPersonId &&
-    props.personStore.getPersonById(props.driveActivity.actorPersonId);
   return (
     <Grid container wrap="nowrap" spacing={2}>
       <Grid item>
-        {person && person.imageUrl && (
-          <Avatar src={person?.imageUrl} style={{ height: 24, width: 24 }} className={classes.icon}>
-            {(person.name || person.id)[0]}
-          </Avatar>
-        )}
+        <img src={props.document.iconLink} className={classes.icon} />
       </Grid>
-      <Grid item>
-        <Typography variant="subtitle2" noWrap>
-          {person && (person.name || person.id)}
-        </Typography>
-        <Typography variant="subtitle2" noWrap>
-          {props.driveActivity.action}
-        </Typography>
+      <Grid item zeroMinWidth>
+        <Link href={`?tab=docs&slug=${props.document.id}`}>
+          <Button className={classes.link} component="a">
+            {props.document.name}
+          </Button>
+        </Link>
+        <br />
         <Typography variant="caption" color="textSecondary">
-          {formatDistanceToNow(new Date(props.driveActivity.time))} ago
+          {formatDistanceToNow(new Date(props.document.updatedAt!))} ago
         </Typography>
       </Grid>
     </Grid>
   );
 };
 
-const DriveActivity = (props: {
+const DriveActivityList = (props: {
   driveActivity: IFormattedDriveActivity[];
   personStore: PersonDataStore;
+  docStore: DocDataStore;
 }) => {
   const classes = useRowStyles();
-  // BADD
-  const activity = uniqBy(props.driveActivity, 'id')
+  const actions = uniqBy(
+    props.driveActivity.filter((action) => action && action.link),
+    'link',
+  );
+  if (actions.length < 1) {
+    return null;
+  }
+  const docs = actions
     .filter((action) => action && action.link)
-    .sort((a, b) => (a.time > b.time ? -1 : 1));
+    .map((action) => props.docStore.getByLink(action.link!))
+    .filter((doc) => doc && doc.updatedAt)
+    .sort((a, b) => (a!.updatedAt! > b!.updatedAt! ? -1 : 1));
   return (
     <div className={classes.root}>
-      {activity.map((item) => (
-        <Activity key={item.id} driveActivity={item} personStore={props.personStore} />
+      {docs.map((doc) => (
+        <Activity key={doc!.id} document={doc!} />
       ))}
     </div>
   );
 };
 
-export default DriveActivity;
+export default DriveActivityList;
