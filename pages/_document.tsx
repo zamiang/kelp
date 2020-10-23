e62c1934e4f718ad3892aee8ea8f34806ecaa2aa
Brennan Moore <brennanmoore@gmail.com>
2020-10-23 11:13:11 -0400

    add google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,6 +3,14 @@ import Document, { Head, Html, Main, NextScript } from 'next/document';
 import React from 'react';
 import theme from '../constants/theme';
 
+const scriptString = `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-2QP6D4R28C');
+`;
+
 export default class MyDocument extends Document {
   render() {
     return (
@@ -12,6 +20,8 @@ export default class MyDocument extends Document {
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-2QP6D4R28C"></script>
+          <script>{scriptString}</script>
           <script src="https://apis.google.com/js/api.js"></script>
         </Head>
         <body>
