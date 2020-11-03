1aa7932cd4065f35b46659b99321a05fa90f29f4
Brennan Moore <brennanmoore@gmail.com>
2020-11-03 14:27:48 -0500

    less broken settings

--- components/user-profile/settings.tsx
+++ components/user-profile/settings.tsx
@@ -21,58 +21,67 @@ const useStyles = makeStyles((theme) => ({
   textField: {
     width: '100%',
   },
+  maxWidth: {
+    maxWidth: 500,
+  },
 }));
 
 const Settings = () => {
-  const styles = panelStyles();
+  const classes = panelStyles();
   const formClasses = useStyles();
   return (
-    <React.Fragment>
-      <div className={styles.row}>
-        <Typography className={styles.title}>Settings</Typography>
-      </div>
-      <div className={styles.row}>
-        <FormControl className={clsx(formClasses.margin, formClasses.textField)}>
-          <InputLabel htmlFor="days-back">Number of days to look back</InputLabel>
-          <Input id="days-back" type={'text'} value={config.NUMBER_OF_DAYS_BACK} />
-        </FormControl>
-        <FormControl className={clsx(formClasses.margin, formClasses.textField)}>
-          <InputLabel htmlFor="week-starts-on">Week starts on</InputLabel>
-          <Input id="week-starts-on" type={'text'} value={config.WEEK_STARTS_ON} />
-        </FormControl>
-        <FormControl className={clsx(formClasses.margin, formClasses.textField)}>
-          <InputLabel htmlFor="week-starts-on">
-            Max meeting attendees to count as interactions in Contact Associates
-          </InputLabel>
-          <Input
-            id="max-meeting-attendees"
-            type={'text'}
-            value={config.MAX_MEETING_ATTENDEE_TO_COUNT_AN_INTERACTION}
-          />
-        </FormControl>
-        <Button
-          variant="contained"
-          color="primary"
-          disableElevation
-          onClick={() => signIn('slack', { callbackUrl: config.REDIRECT_URI })}
-        >
-          Add Slack
-        </Button>
-
-        <div className={styles.row}>
+    <div className={classes.panel}>
+      <div className={classes.section}>
+        <div className={classes.row}>
+          <Typography className={classes.title}>Settings</Typography>
+        </div>
+        <div className={clsx(classes.rowNoBorder, formClasses.maxWidth)}>
+          <FormControl className={clsx(formClasses.margin, formClasses.textField)}>
+            <InputLabel htmlFor="days-back">Number of days to look back</InputLabel>
+            <Input id="days-back" type={'text'} value={config.NUMBER_OF_DAYS_BACK} />
+          </FormControl>
+          <FormControl className={clsx(formClasses.margin, formClasses.textField)}>
+            <InputLabel htmlFor="week-starts-on">Week starts on</InputLabel>
+            <Input id="week-starts-on" type={'text'} value={config.WEEK_STARTS_ON} />
+          </FormControl>
+          <FormControl className={clsx(formClasses.margin, formClasses.textField)}>
+            <InputLabel htmlFor="week-starts-on">
+              Max meeting attendees to count as interactions in Contact Associates
+            </InputLabel>
+            <Input
+              id="max-meeting-attendees"
+              type={'text'}
+              value={config.MAX_MEETING_ATTENDEE_TO_COUNT_AN_INTERACTION}
+            />
+          </FormControl>
+        </div>
+        <div className={clsx(classes.rowNoBorder, formClasses.maxWidth)}>
+          <FormControl className={clsx(formClasses.margin, formClasses.textField)}>
+            <Button
+              variant="contained"
+              color="primary"
+              disableElevation
+              fullWidth
+              onClick={() => signIn('slack', { callbackUrl: config.REDIRECT_URI })}
+            >
+              Add Slack
+            </Button>
+          </FormControl>
+        </div>
+        <div className={clsx(classes.rowNoBorder, formClasses.maxWidth)}>
           <FormControl className={clsx(formClasses.margin, formClasses.textField)}>
             <Button variant="contained" color="primary" disableElevation>
               Save
             </Button>
           </FormControl>
         </div>
-        <div className={styles.row}>
+        <div className={clsx(classes.rowNoBorder, formClasses.maxWidth)}>
           <FormControl className={clsx(formClasses.margin, formClasses.textField)}>
             <LogoutButton />
           </FormControl>
         </div>
       </div>
-    </React.Fragment>
+    </div>
   );
 };
 
