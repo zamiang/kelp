3b60b2ad5af5732f998b2c397276d39cb44e9302
Brennan Moore <brennanmoore@gmail.com>
2020-11-02 22:34:31 -0500

    doh

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,14 +2,13 @@ import { Response } from 'express';
 import NextAuth from 'next-auth';
 import config from '../../../constants/config';
 
-console.log(process.env.MONGO_URL, '<<<<<<<<<< mongo url'
+console.log(process.env.MONGO_URL, '<<<<<<<<<< mongo url');
 
 const options = {
-
   pages: {
     signIn: '/auth/signin',
   },
-  database: `${process.env.MONGO_URL!}/kelp`,
+  database: `mongodb://${process.env.MONGO_URL!}/kelp`,
   jwt: {
     secret: process.env.JWT_SECRET,
     encryption: true,
