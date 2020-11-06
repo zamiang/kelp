3caf2f4cc22afd09bf82dd66075227c0927f521c
Brennan Moore <brennanmoore@gmail.com>
2020-11-06 14:12:01 -0500

    undo safari thing

--- components/store/use-gapi.ts
+++ components/store/use-gapi.ts
@@ -21,6 +21,8 @@ const useGAPI = () => {
       });
       try {
         const isSignedIn = gapi.auth2.getAuthInstance().isSignedIn.get();
+        const isSafari =
+          navigator.userAgent.includes('Safari') && !navigator.userAgent.includes('Chrome');
 
         if (isSignedIn) {
           return setStatus(isSignedIn);
@@ -29,7 +31,7 @@ const useGAPI = () => {
           scope: config.GOOGLE_SCOPES.join(' '),
           redirect_uri: config.REDIRECT_URI,
           // isSignedIn is always false in safari, even after the redirect login method
-          ux_mode: 'redirect',
+          ux_mode: isSafari ? 'popup' : 'redirect',
         });
         if (result) {
           setStatus(true);
