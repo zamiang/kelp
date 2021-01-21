0c43bc10cd7e90d3b3e527ddf761e32450ec5088
Brennan Moore <brennanmoore@gmail.com>
2021-01-21 13:55:26 -0500

    improve fetch until end for drive files

--- components/fetch/fetch-drive-files.ts
+++ components/fetch/fetch-drive-files.ts
@@ -1,33 +1,62 @@
 import { differenceInCalendarDays } from 'date-fns';
+import { last } from 'lodash';
 import config from '../../constants/config';
 
-const fetchDriveFiles = async () => {
-  // Does not allow filtering by modified time OR deleted
-  const driveResponse = await gapi.client.drive.files.list({
-    // includeItemsFromAllDrives: true,
-    // includeTeamDriveItems: true,
-    // supportsAllDrives: true,
-    // supportsTeamDrives: true,
-    orderBy: 'modifiedTime desc',
-    pageSize: 30,
-    fields:
-      'nextPageToken, files(id, name, mimeType, webViewLink, owners, shared, starred, iconLink, trashed, modifiedTime, modifiedByMe, viewedByMe, viewedByMeTime)',
+const currentDate = new Date();
+
+const isFileWithinTimeWindow = (file: gapi.client.drive.File) =>
+  file.modifiedTime &&
+  (!config.SHOULD_FILTER_OUT_FILES_MODIFIED_BEFORE_NUMBER_OF_DAYS_BACK ||
+    differenceInCalendarDays(currentDate, new Date(file.modifiedTime)) <
+      config.NUMBER_OF_DAYS_BACK) &&
+  (!config.SHOULD_FILTER_OUT_FILES_VIEWED_BY_ME_BEFORE_NUMBER_OF_DAYS_BACK ||
+    (file.viewedByMeTime &&
+      differenceInCalendarDays(currentDate, new Date(file.viewedByMeTime)) <
+        config.NUMBER_OF_DAYS_BACK));
+
+const fetchDriveFilePage = (pageToken?: string) =>
+  new Promise((resolve) => {
+    const options = {
+      // includeItemsFromAllDrives: true,
+      // includeTeamDriveItems: true,
+      // supportsAllDrives: true,
+      // supportsTeamDrives: true,
+      orderBy: 'modifiedTime desc',
+      pageSize: 30,
+      fields:
+        'nextPageToken, files(id, name, mimeType, webViewLink, owners, shared, starred, iconLink, trashed, modifiedTime, modifiedByMe, viewedByMe, viewedByMeTime)',
+    } as any;
+    if (pageToken) {
+      options.pageToken = pageToken;
+    }
+    const request = gapi.client.drive.files.list(options);
+    request.execute(resolve);
   });
 
-  return driveResponse && driveResponse.result && driveResponse.result.files
-    ? driveResponse.result.files.filter(
-        (file) =>
-          !file.trashed &&
-          file.modifiedTime &&
-          (!config.SHOULD_FILTER_OUT_FILES_MODIFIED_BEFORE_NUMBER_OF_DAYS_BACK ||
-            differenceInCalendarDays(new Date(), new Date(file.modifiedTime)) <
-              config.NUMBER_OF_DAYS_BACK) &&
-          (!config.SHOULD_FILTER_OUT_FILES_VIEWED_BY_ME_BEFORE_NUMBER_OF_DAYS_BACK ||
-            (file.viewedByMeTime &&
-              differenceInCalendarDays(new Date(), new Date(file.viewedByMeTime)) <
-                config.NUMBER_OF_DAYS_BACK)),
-      )
-    : [];
+const fetchAllDriveFiles = async (results: gapi.client.drive.File[], nextPageToken?: string) => {
+  const driveResponse: any = await fetchDriveFilePage(nextPageToken);
+  const newResults = results.concat(driveResponse.result.files);
+  const isWithinTimeWindow = isFileWithinTimeWindow(last(newResults)!);
+  if (driveResponse.nextPageToken && isWithinTimeWindow) {
+    await fetchAllDriveFiles(newResults, driveResponse.nextPageToken);
+  }
+  return newResults;
+};
+
+const fetchDriveFiles = async () => {
+  const results = await fetchAllDriveFiles([]);
+  return results.filter(
+    (file: gapi.client.drive.File) =>
+      !file.trashed &&
+      file.modifiedTime &&
+      (!config.SHOULD_FILTER_OUT_FILES_MODIFIED_BEFORE_NUMBER_OF_DAYS_BACK ||
+        differenceInCalendarDays(currentDate, new Date(file.modifiedTime)) <
+          config.NUMBER_OF_DAYS_BACK) &&
+      (!config.SHOULD_FILTER_OUT_FILES_VIEWED_BY_ME_BEFORE_NUMBER_OF_DAYS_BACK ||
+        (file.viewedByMeTime &&
+          differenceInCalendarDays(currentDate, new Date(file.viewedByMeTime)) <
+            config.NUMBER_OF_DAYS_BACK)),
+  );
 };
 
 export default fetchDriveFiles;
