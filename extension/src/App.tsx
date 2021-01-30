75c44fb16d51108349f1833ad0d2e5b17a3e574d
Brennan Moore <brennanmoore@gmail.com>
2021-01-30 18:49:50 -0500

    squash

---
+++ extension/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './app.css';
+
+const App = () => (
+  <div className="app">
+    <header className="app-header">
+      <img src={logo} className="app-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <a className="app-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        Learn React
+      </a>
+    </header>
+  </div>
+);
+
+export default App;
