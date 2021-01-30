1db28a8f674be030b2b6c0a06f5be7e97519c55c
Brennan Moore <brennanmoore@gmail.com>
2021-01-30 18:23:27 -0500

    wip

--- extension/src/App.tsx
+++ extension/src/App.tsx
@@ -1,15 +1,15 @@
-import * as React from 'react';
+import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './app.css';
 
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
+  <div className="app">
+    <header className="app-header">
+      <img src={logo} className="app-logo" alt="logo" />
       <p>
         Edit <code>src/App.tsx</code> and save to reload.
       </p>
-      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+      <a className="app-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
         Learn React
       </a>
     </header>
--- extension/src/popup.tsx
+++ extension/src/popup.tsx
@@ -1,7 +1,6 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-
-import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
 import './popup.css';
 
 const mountNode = document.getElementById('popup');
