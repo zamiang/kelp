1115d6c2fd5e411b54d9afe6c80c0f9bc2bd8d22
Brennan Moore <brennanmoore@gmail.com>
2021-01-31 18:34:39 -0500

    remove gapi from chrome bg

--- extension/src/background.ts
+++ extension/src/background.ts
@@ -1,10 +1,8 @@
 import db from '../../components/store/db';
-import useGapi from '../../components/store/use-gapi';
 import getStore from '../../components/store/use-homepage-store';
 
 const init = async () => {
   console.log('starting');
-  useGapi();
   const store = await getStore(await db('extension-test'));
   console.log(store);
 };
