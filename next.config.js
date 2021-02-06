58cc4a4db9cef1dae16d195ba64d94083d7317b7
Brennan Moore <brennanmoore@gmail.com>
2021-02-05 20:26:24 -0500

    wip

--- next.config.js
+++ next.config.js
@@ -1,8 +1,22 @@
 const { createSecureHeaders } = require('next-secure-headers');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = {
   reactStrictMode: true,
   poweredByHeader: false,
+
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    if (process.env.ANALYZE) {
+      config.plugins.push(
+        new BundleAnalyzerPlugin({
+          analyzerMode: 'server',
+          analyzerPort: isServer ? 8888 : 8889,
+          openAnalyzer: true,
+        }),
+      );
+    }
+    return config;
+  },
 
   async rewrites() {
     return [
