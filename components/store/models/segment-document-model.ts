94cd4708aa7f58b6454f7c0982fc12dbe1b15705
Brennan Moore <brennanmoore@gmail.com>
2021-02-14 19:22:26 -0500

    drvie activity item

--- components/store/models/segment-document-model.ts
+++ components/store/models/segment-document-model.ts
@@ -75,7 +75,6 @@ export default class SegmentDocumentModel {
         );
         if (segment) {
           const attendees = await attendeeStore.getAllForSegmentId(segment.id);
-          console.log(attendees, '<<<<<<<', driveActivityItem);
           isActorAttendee = !!attendees.find(
             (a) => a.personGoogleId === driveActivityItem.actorPersonId,
           );
