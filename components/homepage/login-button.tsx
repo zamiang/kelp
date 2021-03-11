742c40d60a81a9ec7775985d0894a9f052abb82d
Brennan Moore <brennanmoore@gmail.com>
2021-03-11 16:19:31 -0500

    doh

--- components/homepage/login-button.tsx
+++ components/homepage/login-button.tsx
@@ -1,11 +1,6 @@
-import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
-import CircularProgress from '@material-ui/core/CircularProgress';
 import { makeStyles } from '@material-ui/core/styles';
-import Link from 'next/link';
-import React, { useEffect, useState } from 'react';
-import { person } from '../fetch/fetch-people';
-import { fetchSelf } from '../fetch/fetch-self';
+import React from 'react';
 
 const useStyles = makeStyles((theme) => ({
   button: {
@@ -24,42 +19,8 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const LoginButton = () => {
-  const [isLoading, setIsLoading] = useState<Boolean>(true);
-  const [currentUser, setCurrentUser] = useState<person | undefined>();
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const accessToken = localStorage.getItem('oauth2');
-      if (accessToken) {
-        const result = await fetchSelf(accessToken);
-        if (result) {
-          setCurrentUser(result);
-        }
-      }
-      setIsLoading(false);
-    };
-    void fetchData();
-  }, []);
-
   const classes = useStyles();
-  if (isLoading) {
-    return <CircularProgress size={22} className={classes.avatar} color="inherit" />;
-  }
 
-  if (currentUser?.id) {
-    return (
-      <Link href="/dashboard">
-        <Button className={classes.button} variant="outlined" disableElevation={true}>
-          <Avatar
-            className={classes.avatar}
-            src={currentUser.imageUrl || undefined}
-            alt={currentUser.emailAddresses[0] || undefined}
-          />
-          My Dashboard
-        </Button>
-      </Link>
-    );
-  }
   return (
     <Button
       onClick={() => (window.location.pathname = '/dashboard')}
