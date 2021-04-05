d9d557a8decee23d7452a7b5eb2512db7058b319
Brennan Moore <brennanmoore@gmail.com>
2021-04-05 15:04:26 -0400

    doh

--- components/dashboard/tasks.tsx
+++ components/dashboard/tasks.tsx
@@ -1,96 +1,13 @@
-import Typography from '@material-ui/core/Typography';
-import { formatDistanceToNow, subDays } from 'date-fns';
-import { sortBy, uniqBy } from 'lodash';
+import Button from '@material-ui/core/Button';
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import { getWeek } from '../shared/date-helpers';
+import CheckIcon from '../../public/icons/check-orange.svg';
+import useButtonStyles from '../shared/button-styles';
 import panelStyles from '../shared/panel-styles';
 import rowStyles from '../shared/row-styles';
-import { ISegment } from '../store/models/segment-model';
 import { ITask } from '../store/models/task-model';
 import { IStore } from '../store/use-store';
 import TaskRow from '../tasks/task-row';
-
-interface IFeaturedTask {
-  taskId: string;
-  task: ITask;
-  meetings: ISegment[];
-  nextMeetingStartAt?: Date;
-  text?: string;
-}
-
-/**
- * Gets documents in the featured section by looking through meetings for the coming week
- * Finds meeetings documents associated with those meetings
- * It sorts in decending order so upcoming meetings are next
- */
-const maxResult = 5;
-const daysToLookBack = 7;
-const getFeaturedTasks = async (props: IStore) => {
-  const currentDate = new Date();
-  const week = getWeek(currentDate);
-  const result = await props.taskDocumentStore.getAllForWeek(week);
-
-  // For tasks edited by the current users that may not be associated with a meeting
-  const tasks = await props.taskStore.getAll();
-  const filterTime = subDays(currentDate, daysToLookBack);
-  const currentUserDocuments = await Promise.all(
-    tasks
-      .filter((item) => item.updatedAt > filterTime && !item.parent)
-      .map(
-        async (task) =>
-          ({
-            taskId: task.id,
-            task,
-            meetings: [] as any,
-            nextMeetingStartsAt: undefined,
-            text: `You edited this task ${formatDistanceToNow(task.updatedAt)} ago`,
-          } as IFeaturedTask),
-      ),
-  );
-
-  // Hash of taskId to meeting array
-  const meetingsForTask: { [id: string]: ISegment[] } = {};
-
-  await Promise.all(
-    result.map(async (r) => {
-      if (!r.segmentId) {
-        return;
-      }
-      const meeting = await props.timeDataStore.getById(r.segmentId);
-      const taskId = r.taskId;
-      if (meeting) {
-        if (taskId && meetingsForTask[taskId]) {
-          meetingsForTask[taskId].push(meeting);
-        } else if (taskId) {
-          meetingsForTask[taskId] = [meeting];
-        }
-      }
-    }),
-  );
-  const d = tasks
-    .map((task) => {
-      const meetings = sortBy(meetingsForTask[task.id], 'start');
-      const nextMeetingStartAt = meetings[0] ? meetings[0].start : undefined;
-      const text =
-        meetings[0] && nextMeetingStartAt
-          ? `${meetings[0].summary} ${formatDistanceToNow(nextMeetingStartAt)} ago`
-          : undefined;
-      return {
-        taskId: task.id,
-        task,
-        meetings,
-        nextMeetingStartAt,
-        text,
-      } as IFeaturedTask;
-    })
-    .filter((m) => m.nextMeetingStartAt);
-
-  return uniqBy(sortBy(d, 'nextMeetingStartAt').concat(currentUserDocuments), 'taskId').slice(
-    0,
-    maxResult,
-  );
-};
 
 const AllTasks = (props: {
   store: IStore;
@@ -98,8 +15,8 @@ const AllTasks = (props: {
   setTaskId?: (id: string) => void;
 }) => {
   const classes = rowStyles();
+  const buttonClasses = useButtonStyles();
   const [tasks, setTasks] = useState<ITask[]>([]);
-  const [topTasks, setTopTasks] = useState<IFeaturedTask[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -110,34 +27,22 @@ const AllTasks = (props: {
     void fetchData();
   }, [props.store.lastUpdated, props.store.isLoading]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const featuredTasks = await getFeaturedTasks(props.store);
-      setTopTasks(featuredTasks);
-      if (featuredTasks[0] && featuredTasks[0].taskId && props.setTaskId) {
-        props.setTaskId(featuredTasks[0].taskId);
-      }
-    };
-    void fetchData();
-  }, [props.store.lastUpdated, props.store.isLoading]);
-
   return (
     <React.Fragment>
-      {topTasks.length > 0 && (
-        <div className={classes.rowHighlight}>
-          <Typography className={classes.rowText} variant="h6">
-            Recent tasks
-          </Typography>
-          {topTasks.map((task) => (
-            <TaskRow
-              key={task.task.id}
-              task={task.task}
-              store={props.store}
-              selectedTaskId={props.selectedTaskId}
-            />
-          ))}
-        </div>
-      )}
+      <div className={classes.rowNoHover}>
+        <Button
+          className={buttonClasses.button}
+          variant="outlined"
+          disableElevation
+          color="primary"
+          startIcon={<CheckIcon width="24" height="24" />}
+          onClick={() => alert('wtf')}
+          style={{ width: 'auto', margin: '0 auto' }}
+        >
+          Add a task
+        </Button>
+      </div>
+
       <div>
         {tasks.map((task) => (
           <TaskRow
---
+++ components/tasks/featured-tasks.ts
@@ -0,0 +1,86 @@
+import { formatDistanceToNow, subDays } from 'date-fns';
+import { sortBy, uniqBy } from 'lodash';
+import { getWeek } from '../shared/date-helpers';
+import { ISegment } from '../store/models/segment-model';
+import { ITask } from '../store/models/task-model';
+import { IStore } from '../store/use-store';
+
+interface IFeaturedTask {
+  taskId: string;
+  task: ITask;
+  meetings: ISegment[];
+  nextMeetingStartAt?: Date;
+  text?: string;
+}
+
+/**
+ * Gets documents in the featured section by looking through meetings for the coming week
+ * Finds meeetings documents associated with those meetings
+ * It sorts in decending order so upcoming meetings are next
+ */
+const maxResult = 5;
+const daysToLookBack = 7;
+export const getFeaturedTasks = async (props: IStore) => {
+  const currentDate = new Date();
+  const week = getWeek(currentDate);
+  const result = await props.taskDocumentStore.getAllForWeek(week);
+
+  // For tasks edited by the current users that may not be associated with a meeting
+  const tasks = await props.taskStore.getAll();
+  const filterTime = subDays(currentDate, daysToLookBack);
+  const currentUserDocuments = await Promise.all(
+    tasks
+      .filter((item) => item.updatedAt > filterTime && !item.parent)
+      .map(
+        async (task) =>
+          ({
+            taskId: task.id,
+            task,
+            meetings: [] as any,
+            nextMeetingStartsAt: undefined,
+            text: `You edited this task ${formatDistanceToNow(task.updatedAt)} ago`,
+          } as IFeaturedTask),
+      ),
+  );
+  // Hash of taskId to meeting array
+  const meetingsForTask: { [id: string]: ISegment[] } = {};
+
+  await Promise.all(
+    result.map(async (r) => {
+      if (!r.segmentId) {
+        return;
+      }
+      const meeting = await props.timeDataStore.getById(r.segmentId);
+      const taskId = r.taskId;
+      if (meeting) {
+        if (taskId && meetingsForTask[taskId]) {
+          meetingsForTask[taskId].push(meeting);
+        } else if (taskId) {
+          meetingsForTask[taskId] = [meeting];
+        }
+      }
+    }),
+  );
+  const d = tasks
+    .map((task) => {
+      const meetings = sortBy(meetingsForTask[task.id], 'start');
+      const nextMeetingStartAt = meetings[0] ? meetings[0].start : undefined;
+      const text =
+        meetings[0] && nextMeetingStartAt
+          ? `${meetings[0].summary} ${formatDistanceToNow(nextMeetingStartAt)} ago`
+          : undefined;
+      return {
+        taskId: task.id,
+        task,
+        meetings,
+        nextMeetingStartAt,
+        text,
+      } as IFeaturedTask;
+    })
+    .filter((m) => m.nextMeetingStartAt);
+
+  return uniqBy(sortBy(d, 'nextMeetingStartAt').concat(currentUserDocuments), 'taskId').slice(
+    0,
+    maxResult,
+  );
+};
