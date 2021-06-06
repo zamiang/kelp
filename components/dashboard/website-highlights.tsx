90c30345363160474c98945f49978d38852e70c5
Brennan Moore <brennanmoore@gmail.com>
2021-06-06 15:58:48 -0400

    wip

--- components/dashboard/website-highlights.tsx
+++ components/dashboard/website-highlights.tsx
@@ -1,6 +1,9 @@
+import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
 import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 import { subDays } from 'date-fns';
 import { uniqBy } from 'lodash';
 import React, { useEffect, useState } from 'react';
@@ -84,63 +87,76 @@ const getFeaturedWebsites = async (props: IStore) => {
     .slice(0, maxResult);
 };
 
+const useStyles = makeStyles((theme) => ({
+  container: {
+    padding: theme.spacing(),
+    background: theme.palette.background.paper,
+    opacity: 1,
+    transition: 'opacity 0.3s',
+    '&:hover': {
+      opacity: 0.8,
+    },
+  },
+  dots: {
+    backgroundImage:
+      'radial-gradient(rgba(250, 250, 250, 0.5) 20%, transparent 20%), radial-gradient(rgba(250, 250, 250, 0.5) 20%, transparent 20%)',
+    backgroundPosition: '0 0, 5px 5px',
+    backgroundSize: '3px 3px',
+    backgroundRepeat: 'repeat',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+  },
+  imageContainer: {
+    backgroundSize: 'cover',
+    display: 'block',
+    paddingBottom: '66%',
+    overflow: 'hidden',
+    height: 0,
+    position: 'relative',
+  },
+}));
+
 const LargeWebsite = (props: { store: IStore; item: IFeaturedWebsite }) => {
   const [image, setImage] = useState<IWebsiteImage>();
+  const classes = useStyles();
 
   useEffect(() => {
     const fetchData = async () => {
       const i = await props.store.websiteImageStore.getById(props.item.websiteId);
-      console.log(i, '<<<<image<', props.item.websiteId);
       setImage(i);
     };
     void fetchData();
   }, []);
 
-  if (!image) {
-    return null;
-  }
-
   return (
     <Grid item xs={3}>
-      <a
-        href={props.item.websiteId}
-        style={{
-          display: 'block',
-          paddingBottom: '66%',
-          overflow: 'hidden',
-          height: 0,
-          background: '/dots.png',
-          position: 'relative',
-          marginBottom: 8,
-        }}
-      >
-        {image && (
-          <img
-            src={image?.image}
-            style={{
-              maxWidth: '100%',
-            }}
-          />
-        )}
-        <div
-          style={{
-            backgroundImage:
-              'radial-gradient(rgba(250, 250, 250, 0.5) 20%, transparent 20%), radial-gradient(rgba(250, 250, 250, 0.5) 20%, transparent 20%)',
-            backgroundPosition: '0 0, 5px 5px',
-            backgroundSize: '3px 3px',
-            backgroundRepeat: 'repeat',
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            width: '100%',
-            height: '100%',
-          }}
-        ></div>
-      </a>
-      <Link href={props.item.websiteId}>
-        <Typography variant="h4" noWrap>
-          {props.item.text}
-        </Typography>
+      <Link href={props.item.websiteId} underline="none">
+        <Box boxShadow={1} borderRadius={16} className={classes.container}>
+          <Grid container alignItems="center">
+            <Grid item>
+              <IconButton size="small">
+                <img
+                  src={`chrome://favicon/size/48@1x/${props.item.websiteId}`}
+                  height="18"
+                  width="18"
+                  style={{ margin: '0 auto' }}
+                />
+              </IconButton>
+            </Grid>
+            <Grid item zeroMinWidth xs>
+              <Typography noWrap>{props.item.text}</Typography>
+            </Grid>
+          </Grid>
+          <div
+            className={classes.imageContainer}
+            style={{ backgroundImage: `url('${image?.image}')` }}
+          >
+            <div className={classes.dots}></div>
+          </div>
+        </Box>
       </Link>
     </Grid>
   );
