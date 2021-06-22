48fd390f7b1f1b03a429b36199e15aadf9489bdc
Brennan Moore <brennanmoore@gmail.com>
2021-06-22 11:58:44 -0400

    popup improvements

--- components/website/hide-url-dialog.tsx
+++ components/website/hide-url-dialog.tsx
@@ -5,22 +5,31 @@ import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import React from 'react';
-import { boxShadow } from '../../constants/theme';
 import CloseIcon from '../../public/icons/close.svg';
 
 const useStyles = makeStyles((theme) => ({
   dialogContent: {
-    padding: theme.spacing(8),
+    padding: theme.spacing(6),
+    position: 'relative',
   },
   button: {
     textDecoration: 'none',
     cursor: 'pointer',
-    boxShadow,
-    borderRadius: 20,
+    borderRadius: 33,
     background: theme.palette.background.paper,
     color: theme.palette.primary.main,
     paddingRight: theme.spacing(3),
     paddingLeft: theme.spacing(3),
+    display: 'block',
+    width: '100%',
+    paddingTop: 12,
+    paddingBottom: 12,
+    marginTop: theme.spacing(2),
+  },
+  closeButton: {
+    position: 'absolute',
+    top: 42,
+    right: 42,
   },
 }));
 
@@ -42,36 +51,39 @@ export const HideUrlDialog = (props: {
         <div className={classes.dialogContent}>
           <Grid container justify="space-between">
             <Grid item>
-              <Typography variant="h3" noWrap>
-                No longer recommend this website
+              <Typography variant="h3">No longer recommend this website</Typography>
+              <br />
+              <Typography color="textSecondary">
+                Don’t worry, you can always undo via settings.
               </Typography>
-            </Grid>
-            <Grid item>
-              <IconButton onClick={() => props.setHideDialogUrl(undefined)}>
+              <br />
+              <IconButton
+                onClick={() => props.setHideDialogUrl(undefined)}
+                className={classes.closeButton}
+              >
                 <CloseIcon width="24" height="24" />
               </IconButton>
             </Grid>
           </Grid>
           <Grid container alignItems="center" justify="space-between">
-            <Grid item xs={5}>
+            <Grid item xs={12}>
               <Button
                 disableElevation={false}
-                onClick={() => props.hideDialogDomain && props.hideDomain(props.hideDialogDomain)}
-                className={classes.button}
-              >
-                Hide all from {props.hideDialogDomain}
-              </Button>
-            </Grid>
-            <Grid item>
-              <Typography variant="h3">OR</Typography>
-            </Grid>
-            <Grid item xs={5}>
-              <Button
-                disableElevation={false}
+                variant="outlined"
                 onClick={() => props.hideDialogUrl && props.hideUrl(props.hideDialogUrl)}
                 className={classes.button}
               >
                 Hide this website
+              </Button>
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                disableElevation={false}
+                variant="outlined"
+                onClick={() => props.hideDialogDomain && props.hideDomain(props.hideDialogDomain)}
+                className={classes.button}
+              >
+                Hide all from {props.hideDialogDomain}
               </Button>
             </Grid>
           </Grid>
