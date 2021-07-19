95332b7b88ceed60c2c841a7ba0d712cfbdb925e
Brennan Moore <brennanmoore@gmail.com>
2021-07-19 18:23:56 -0400

    search improvementsg

--- components/nav/search-bar.tsx
+++ components/nav/search-bar.tsx
@@ -1,8 +1,11 @@
+import { IconButton } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
+import CloseIconOrange from '../../public/icons/close-orange.svg';
+import CloseIcon from '../../public/icons/close.svg';
 import SearchIconOrange from '../../public/icons/search-orange.svg';
 import SearchIconWhite from '../../public/icons/search-white.svg';
 import SearchIcon from '../../public/icons/search.svg';
@@ -23,12 +26,17 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const searchInputId = 'searchInput';
+
 const SearchBar = (props: { isDarkMode: boolean }) => {
   const classes = useStyles();
   const router = useHistory();
   const location = useLocation();
+  const search = location.search;
+  const [value, setValue] = useState('');
 
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setValue(e.target.value);
     void router.push(`/search?query=${e.target.value}`);
   };
 
@@ -55,17 +63,35 @@ const SearchBar = (props: { isDarkMode: boolean }) => {
           type="text"
           placeholder="Search…"
           fullWidth
+          id={searchInputId}
           autoComplete="off"
           autoFocus={true}
           onChange={handleChange}
           name="query"
           margin="dense"
+          value={value}
           InputProps={{
             className: classes.input,
             disableUnderline: true,
           }}
         />
       </Grid>
+      {search && (
+        <Grid item>
+          <IconButton
+            onClick={() => {
+              router.push('/home');
+              setValue('');
+            }}
+          >
+            {props.isDarkMode ? (
+              <CloseIconOrange width="24" height="24" />
+            ) : (
+              <CloseIcon width="24" height="24" />
+            )}
+          </IconButton>
+        </Grid>
+      )}
     </Grid>
   );
 };
