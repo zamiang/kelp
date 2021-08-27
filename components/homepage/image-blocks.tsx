0a09cf6db6bca4ff129c0c47709383d89ec097eb
Brennan Moore <brennanmoore@gmail.com>
2021-08-27 14:27:05 -0400

    use nextjs image tag

--- components/homepage/image-blocks.tsx
+++ components/homepage/image-blocks.tsx
@@ -2,13 +2,18 @@ import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import makeStyles from '@material-ui/core/styles/makeStyles';
+import Image from 'next/image';
 import React from 'react';
+import meetingGif from '../../public/animations/meetings.gif';
+import pinGif from '../../public/animations/pin.gif';
+import tagGif from '../../public/animations/tags.gif';
 
 export const useStyles = makeStyles((theme) => ({
   image: {
     margin: '0px auto',
     display: 'block',
     borderRadius: 20,
+    maxWidth: '100%',
   },
   heading: {
     fontSize: 24,
@@ -39,19 +44,31 @@ export const ImageBlocks = () => {
           <Typography variant="h4" className={classes.heading}>
             Automatically associate web pages with meetings
           </Typography>
-          <img src="/animations/meetings.gif" className={classes.image} />
+          <Image
+            src={meetingGif}
+            className={classes.image}
+            alt="If you view a web apge during a meeting, it will be associated with the meetings"
+          />
         </Grid>
         <Grid item>
           <Typography variant="h4" className={classes.heading}>
             Smart tags that automatically group web pages
           </Typography>
-          <img src="/animations/tags.gif" className={classes.image} />
+          <Image
+            src={tagGif}
+            className={classes.image}
+            alt="Use smart tags to automatically group web pages"
+          />
         </Grid>
         <Grid item>
           <Typography variant="h4" className={classes.heading}>
             Reorder as needed
           </Typography>
-          <img src="/animations/pin.gif" className={classes.image} />
+          <Image
+            src={pinGif}
+            className={classes.image}
+            alt="Reorder webpages by clicking the pin icon"
+          />
         </Grid>
       </Grid>
     </Container>
