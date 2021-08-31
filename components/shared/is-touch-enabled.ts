eb4257ec47d4ed57ddf06b0c96ab329d7769f588
Brennan Moore <brennanmoore@gmail.com>
2021-08-31 12:09:32 -0400

    maybe fix

--- components/shared/is-touch-enabled.ts
+++ components/shared/is-touch-enabled.ts
@@ -1,4 +1,6 @@
 const isTouchEnabled = () =>
-  'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
+  'ontouchstart' in window ||
+  navigator.maxTouchPoints > 0 ||
+  (navigator as any).msMaxTouchPoints > 0;
 
 export default isTouchEnabled;
