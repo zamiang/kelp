d78be16b4bbbaf10364599dcb9e88f6372bba4a7
Brennan Moore <brennanmoore@gmail.com>
2021-10-08 12:02:52 -0400

    remove document metadata

--- components/homepage/install-ui-blocks.tsx
+++ components/homepage/install-ui-blocks.tsx
@@ -69,25 +69,6 @@ const InstallUiBlocks = () => (
         <img src="google-permissions/calendar.png" className={classes.image} />
       </Grid>
     </Grid>
-    <Grid container className={classes.section} alignItems="center">
-      <Grid item md={6} sm={12} className={classes.sectionText}>
-        <Typography variant="h4" className={classes.heading}>
-          <div className={classes.dot}></div>Read Document Events
-        </Typography>
-        <Typography>
-          This allows Kelp minimal access to event data like if someone edits or comments on a
-          document. Document event data helps Kelp give you more accurate recommendations.
-        </Typography>
-      </Grid>
-      <Grid
-        item
-        md={6}
-        sm={12}
-        className={clsx(classes.sectionImageRightBottom, classes.greyContainer)}
-      >
-        <img src="google-permissions/activity.png" className={classes.image} />
-      </Grid>
-    </Grid>
   </StyledContainer>
 );
 
